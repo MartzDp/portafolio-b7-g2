dbf09232ccc67ead8de0ccdbe62d01c324ba902e
MartzDp <150105191+MartzDp@users.noreply.github.com>
2024-08-08 19:06:41 -0600

    Uso de JavaScript
    Se realizo un efecto typewriter.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//alert("Hola Mundo <3");
-
+//alert("Hola Mundo");
 
 let app = document.getElementById('app');
 
@@ -15,6 +14,25 @@ typewriter
   .typeString('Diana Martínez')
   .pauseFor(300)
   .deleteAll()
-  .typeString('Desarrolladora FrontEnd Jr')
+  .typeString('Desarrolladora FrontEnd Jr.')
+  .pauseFor(1000)
+  .start();
+
+
+  let frase = document.getElementById('frase');
+
+let typewriterFrase = new Typewriter(frase, {
+  loop: true,
+  delay: 75,
+});
+
+//reconoces una acción porque tiene paréntesis, después del nombre de la acción 
+//a veces tiene el operador punto
+typewriterFrase
+  .pauseFor(2500) //milisegundos 2.5 segundos
+  .typeString('"El éxito no se logra solo con cualidades especiales. Es, sobre todo, un trabajo de constancia, de método y de organización."')
+  .pauseFor(300)
+  .deleteAll()
+  .typeString('Víctor Hugo.')
   .pauseFor(1000)
   .start();
